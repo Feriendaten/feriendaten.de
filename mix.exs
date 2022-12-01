b35a3fff73125741c513609614b2d8dc20f45d9e
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2022-12-01 10:15:22 +0100

    Bump version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Feriendaten.MixProject do
   def project do
     [
       app: :feriendaten,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
