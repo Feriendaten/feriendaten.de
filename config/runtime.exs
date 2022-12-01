63bf1b1d3351fc7c983ac6ebe5fcb937d7a9402b
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2022-12-01 17:20:03 +0100

    http prod server configuration

--- config/runtime.exs
+++ config/runtime.exs
@@ -66,14 +66,15 @@ if config_env() == :prod do
 
   config :feriendaten, FeriendatenWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
-    ],
+    # http: [
+    #   # Enable IPv6 and bind on all interfaces.
+    #   # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+    #   # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+    #   # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+    #   ip: {0, 0, 0, 0, 0, 0, 0, 0},
+    #   port: port
+    # ],
+    http: [ip: {127, 0, 0, 1}, port: 4010],
     secret_key_base: secret_key_base
 
   # ## SSL Support
