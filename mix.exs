a80eeeaf7f18590df07ed7966c322b0f75de5b65
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2022-12-05 18:02:51 +0100

    new version 0.2.16

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Feriendaten.MixProject do
   def project do
     [
       app: :feriendaten,
-      version: "0.2.15",
+      version: "0.2.16",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
