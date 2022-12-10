cb80d2b894ff651dd9c598ec17b2e9c8f6635d90
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2022-12-10 16:54:05 +0100

    Show version number in the footer.

---
+++ lib/feriendaten_web/helpers/view_helpers.ex
@@ -0,0 +1,9 @@
+defmodule FeriendatenWeb.ViewHelpers do
+  @moduledoc """
+  Helper functions for use with views.
+  """
+
+  @version Mix.Project.config()[:version]
+
+  def version, do: @version
+end
