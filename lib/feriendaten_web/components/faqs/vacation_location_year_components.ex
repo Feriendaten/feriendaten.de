e398a2418fc8a8960c7ba6d1e5922121855b7827
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2022-12-12 13:32:37 +0100

    Add FAQ schema

--- lib/feriendaten_web/components/faqs/vacation_location_year_components.ex
+++ lib/feriendaten_web/components/faqs/vacation_location_year_components.ex
@@ -22,30 +22,40 @@ defmodule FeriendatenWeb.VacationLocationYearComponents do
           <h2 class="mb-8 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">
             FAQ
           </h2>
-          <div class="grid pt-8 text-left border-t border-gray-200 md:gap-16 dark:border-gray-700 md:grid-cols-2">
+          <div
+            class="grid pt-8 text-left border-t border-gray-200 md:gap-16 dark:border-gray-700 md:grid-cols-2"
+            itemscope
+            itemtype="https://schema.org/FAQPage"
+          >
             <%= for faq_entries <- [@first_half, @second_half] do %>
               <div>
                 <%= for faq_entry <- faq_entries do %>
-                  <div class="mb-10">
-                    <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
-                      <svg
-                        class="flex-shrink-0 w-5 h-5 mr-2 text-gray-500 dark:text-gray-400"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                          clip-rule="evenodd"
+                  <div itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+                    <div class="mb-10">
+                      <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                        <svg
+                          class="flex-shrink-0 w-5 h-5 mr-2 text-gray-500 dark:text-gray-400"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
                         >
-                        </path>
-                      </svg>
-                      <%= faq_entry[:question] %>
-                    </h3>
-                    <p class="text-gray-500 dark:text-gray-400">
-                      <%= faq_entry[:answer] %>
-                    </p>
+                          <path
+                            fill-rule="evenodd"
+                            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                            clip-rule="evenodd"
+                          >
+                          </path>
+                        </svg>
+                        <div itemprop="name">
+                          <%= faq_entry[:question] %>
+                        </div>
+                      </h3>
+                      <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                        <p class="text-gray-500 dark:text-gray-400" itemprop="text">
+                          <%= faq_entry[:answer] %>
+                        </p>
+                      </div>
+                    </div>
                   </div>
                 <% end %>
               </div>
---
+++ test/feriendaten_web/controllers/faq_test.exs
@@ -0,0 +1,213 @@
+defmodule FeriendatenWeb.FaqTest do
+  use FeriendatenWeb.ConnCase
+
+  alias Feriendaten.Maps
+
+  test "GET /", %{conn: conn} do
+    {:ok, land} =
+      Maps.create_level(%{
+        name: "Land",
+        order: 1
+      })
+
+    {:ok, bundesland} =
+      Maps.create_level(%{
+        name: "Bundesland",
+        order: 2
+      })
+
+    {:ok, deutschland} =
+      Maps.create_location(%{
+        name: "Deutschland",
+        code: "DE",
+        level_id: land.id,
+        is_active: true
+      })
+
+    {:ok, hessen} =
+      Maps.create_location(%{
+        name: "Hessen",
+        code: "HE",
+        level_id: bundesland.id,
+        parent_id: deutschland.id,
+        is_active: true
+      })
+
+    {:ok, bayern} =
+      Maps.create_location(%{
+        name: "Bayern",
+        code: "BY",
+        level_id: bundesland.id,
+        parent_id: deutschland.id,
+        is_active: true
+      })
+
+    {:ok, weihnachtsferien} =
+      Feriendaten.Calendars.create_vacation(%{
+        name: "Weihnachten",
+        colloquial: "Weihnachtsferien",
+        listed: true,
+        for_students: true,
+        for_everybody: false,
+        priority: 5,
+        public_holiday: false,
+        school_vacation: true
+      })
+
+    {:ok, osterferien} =
+      Feriendaten.Calendars.create_vacation(%{
+        name: "Ostern",
+        colloquial: "Osterferien",
+        listed: true,
+        for_students: true,
+        for_everybody: false,
+        priority: 5,
+        public_holiday: false,
+        school_vacation: true
+      })
+
+    {:ok, sommerferien} =
+      Feriendaten.Calendars.create_vacation(%{
+        name: "Sommer",
+        colloquial: "Sommerferien",
+        listed: true,
+        for_students: true,
+        for_everybody: false,
+        priority: 5,
+        public_holiday: false,
+        school_vacation: true
+      })
+
+    {:ok, herbstferien} =
+      Feriendaten.Calendars.create_vacation(%{
+        name: "Herbst",
+        colloquial: "Herbstferien",
+        listed: true,
+        for_students: true,
+        for_everybody: false,
+        priority: 5,
+        public_holiday: false,
+        school_vacation: true
+      })
+
+    {:ok, _entry} =
+      Feriendaten.Calendars.create_entry(%{
+        starts_on: ~D[2039-12-22],
+        ends_on: ~D[2040-01-05],
+        vacation_id: weihnachtsferien.id,
+        location_id: hessen.id,
+        listed: true,
+        for_students: true,
+        for_everybody: false,
+        priority: 5,
+        public_holiday: false,
+        school_vacation: true
+      })
+
+    {:ok, _entry} =
+      Feriendaten.Calendars.create_entry(%{
+        starts_on: ~D[2040-04-01],
+        ends_on: ~D[2040-04-21],
+        vacation_id: osterferien.id,
+        location_id: hessen.id,
+        listed: true,
+        for_students: true,
+        for_everybody: false,
+        priority: 5,
+        public_holiday: false,
+        school_vacation: true
+      })
+
+    {:ok, _entry} =
+      Feriendaten.Calendars.create_entry(%{
+        starts_on: ~D[2040-07-20],
+        ends_on: ~D[2040-08-04],
+        vacation_id: sommerferien.id,
+        location_id: hessen.id,
+        listed: true,
+        for_students: true,
+        for_everybody: false,
+        priority: 5,
+        public_holiday: false,
+        school_vacation: true
+      })
+
+    {:ok, _entry} =
+      Feriendaten.Calendars.create_entry(%{
+        starts_on: ~D[2040-10-01],
+        ends_on: ~D[2040-10-07],
+        vacation_id: herbstferien.id,
+        location_id: hessen.id,
+        listed: true,
+        for_students: true,
+        for_everybody: false,
+        priority: 5,
+        public_holiday: false,
+        school_vacation: true
+      })
+
+    {:ok, _entry} =
+      Feriendaten.Calendars.create_entry(%{
+        starts_on: ~D[2040-10-01],
+        ends_on: ~D[2040-10-14],
+        vacation_id: herbstferien.id,
+        location_id: bayern.id,
+        listed: true,
+        for_students: true,
+        for_everybody: false,
+        priority: 5,
+        public_holiday: false,
+        school_vacation: true
+      })
+
+    {:ok, _entry} =
+      Feriendaten.Calendars.create_entry(%{
+        starts_on: ~D[2040-04-01],
+        ends_on: ~D[2040-04-14],
+        vacation_id: osterferien.id,
+        location_id: bayern.id,
+        listed: true,
+        for_students: true,
+        for_everybody: false,
+        priority: 5,
+        public_holiday: false,
+        school_vacation: true
+      })
+
+    {:ok, _entry} =
+      Feriendaten.Calendars.create_entry(%{
+        starts_on: ~D[2039-12-22],
+        ends_on: ~D[2040-01-10],
+        vacation_id: weihnachtsferien.id,
+        location_id: bayern.id,
+        listed: true,
+        for_students: true,
+        for_everybody: false,
+        priority: 5,
+        public_holiday: false,
+        school_vacation: true
+      })
+
+    conn = get(conn, ~p"/herbstferien/hessen/2040?datum=2040-01-01")
+    assert html_response(conn, 200) =~ "Herbstferien Hessen 2040"
+    assert html_response(conn, 200) =~ "Herbstferien"
+    assert html_response(conn, 200) =~ "01.10. - 07.10."
+    assert html_response(conn, 200) =~ "FAQ"
+    assert html_response(conn, 200) =~ "Warum Herbstferien kürzer?"
+    assert html_response(conn, 200) =~ "Warum nur 1 Woche Herbstferien Hessen?"
+    assert html_response(conn, 200) =~ "Wann sind 2040 Herbstferien in Hessen?"
+
+    assert html_response(conn, 200) =~
+             "Die Herbstferien in Hessen beginnen am 01.10.2040 (ein Montag). Bis dahin sind es noch 274 Tage."
+
+    assert html_response(conn, 200) =~ "Wann beginnen die Herbstferien in Hessen?"
+
+    conn = get(conn, ~p"/herbstferien/bayern/2040?datum=2040-01-01")
+    assert html_response(conn, 200) =~ "Herbstferien Bayern 2040"
+    assert html_response(conn, 200) =~ "Herbstferien"
+    assert html_response(conn, 200) =~ "01.10. - 14.10."
+    assert html_response(conn, 200) =~ "FAQ"
+    refute html_response(conn, 200) =~ "Warum Herbstferien kürzer?"
+    refute html_response(conn, 200) =~ "Warum nur 1 Woche Herbstferien Hessen?"
+  end
+end
