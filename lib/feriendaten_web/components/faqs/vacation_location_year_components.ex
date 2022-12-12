69223f6abd20ca813d44b5233b82f301f512b3b7
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2022-12-12 07:56:46 +0100

    Refactoring FAQ

--- lib/feriendaten_web/components/faqs/vacation_location_year_components.ex
+++ lib/feriendaten_web/components/faqs/vacation_location_year_components.ex
@@ -13,81 +13,9 @@ defmodule FeriendatenWeb.VacationLocationYearComponents do
   attr :requested_date, :any, required: true
 
   def vacation_location_year_faq(assigns) do
-    vacation_colloquial = hd(assigns.entries) |> Map.get(:colloquial)
+    assigns = enrich_with_faq_entries(assigns)
 
-    faq_entries = []
-
-    faq_entries = [
-      FederalStateFaq.wann_sind_in_location_year_vaction?(
-        assigns.location,
-        assigns.year,
-        assigns.entries
-      )
-      | faq_entries
-    ]
-
-    faq_entries =
-      warum_drei_wochen_osterferien(
-        faq_entries,
-        vacation_colloquial,
-        assigns
-      )
-
-    faq_entries =
-      warum_nur_eine_woche_herbstferien(
-        faq_entries,
-        vacation_colloquial,
-        assigns
-      )
-
-    preposition =
-      case assigns.location.name do
-        "Saarland" -> "im"
-        _ -> "in"
-      end
-
-    first_hit =
-      Enum.find(assigns.entries, fn entry -> entry.colloquial == vacation_colloquial end)
-
-    bonus_answer =
-      case Date.compare(first_hit.starts_on, assigns.requested_date) do
-        :gt ->
-          "Bis dahin sind es noch #{Date.diff(first_hit.starts_on, assigns.requested_date)} Tage."
-
-        _ ->
-          ""
-      end
-
-    faq_entries = [
-      %{
-        question:
-          "Wann beginnen die #{vacation_colloquial} #{preposition} #{assigns.location.name}?",
-        answer:
-          "Die #{vacation_colloquial} #{preposition} #{assigns.location.name} beginnen am #{Calendar.strftime(first_hit.starts_on, "%d.%m.%Y")} (ein #{FeriendatenWeb.LocationYearFaqComponents.wochentag(first_hit.starts_on)}). #{bonus_answer}"
-      }
-      | faq_entries
-    ]
-
-    faq_entries = [
-      %{
-        question:
-          "Wann sind #{assigns.year} #{vacation_colloquial} #{preposition} #{assigns.location.name}?",
-        answer:
-          "Die #{vacation_colloquial} #{preposition} #{assigns.location.name} beginnen am #{Calendar.strftime(first_hit.starts_on, "%d.%m.%Y")} (ein #{FeriendatenWeb.LocationYearFaqComponents.wochentag(first_hit.starts_on)}). #{bonus_answer}"
-      }
-      | faq_entries
-    ]
-
-    unless faq_entries == [] do
-      addon = rem(length(faq_entries), 2)
-
-      half = length(faq_entries) |> div(2)
-      {first_half, second_half} = Enum.split(faq_entries, half + addon)
-
-      assigns = assign(assigns, :faq_entries, faq_entries)
-      assigns = assign(assigns, :faq_entries_first_half, first_half)
-      assigns = assign(assigns, :faq_entries_second_half, second_half)
-
+    unless assigns.faq_entries == [] do
       ~H"""
       <section class="bg-white dark:bg-gray-900">
         <div class="pt-8">
@@ -95,7 +23,7 @@ defmodule FeriendatenWeb.VacationLocationYearComponents do
             FAQ
           </h2>
           <div class="grid pt-8 text-left border-t border-gray-200 md:gap-16 dark:border-gray-700 md:grid-cols-2">
-            <%= for faq_entries <- [@faq_entries_first_half, @faq_entries_second_half] do %>
+            <%= for faq_entries <- [@first_half, @second_half] do %>
               <div>
                 <%= for faq_entry <- faq_entries do %>
                   <div class="mb-10">
@@ -135,9 +63,10 @@ defmodule FeriendatenWeb.VacationLocationYearComponents do
 
   def warum_drei_wochen_osterferien(
         faq_entries,
-        vacation_colloquial = "Osterferien",
         assigns
       ) do
+    vacation_colloquial = hd(assigns.entries) |> Map.get(:colloquial)
+
     first_hit =
       Enum.find(assigns.entries, fn entry -> entry.colloquial == vacation_colloquial end)
 
@@ -189,6 +118,97 @@ defmodule FeriendatenWeb.VacationLocationYearComponents do
   end
 
   def warum_nur_eine_woche_herbstferien(faq_entries, _, _) do
+    faq_entries
+  end
+
+  defp first_half(faq_entries) do
+    addon = rem(length(faq_entries), 2)
+
+    half = length(faq_entries) |> div(2)
+    {first_half, _second_half} = Enum.split(faq_entries, half + addon)
+    first_half
+  end
+
+  defp second_half(faq_entries) do
+    addon = rem(length(faq_entries), 2)
+
+    half = length(faq_entries) |> div(2)
+    {_first_half, second_half} = Enum.split(faq_entries, half + addon)
+    second_half
+  end
+
+  defp enrich_with_faq_entries(assigns) do
+    assigns = Map.put(assigns, :faq_entries, list_of_faq_entries(assigns))
+    assigns = Map.put(assigns, :first_half, first_half(assigns.faq_entries))
+    assigns = Map.put(assigns, :second_half, second_half(assigns.faq_entries))
+    assigns
+  end
+
+  defp list_of_faq_entries(assigns) do
+    vacation_colloquial = hd(assigns.entries) |> Map.get(:colloquial)
+
+    faq_entries = []
+
+    faq_entries = [
+      FederalStateFaq.wann_sind_in_location_year_vaction?(
+        assigns.location,
+        assigns.year,
+        assigns.entries
+      )
+      | faq_entries
+    ]
+
+    faq_entries =
+      warum_drei_wochen_osterferien(
+        faq_entries,
+        assigns
+      )
+
+    faq_entries =
+      warum_nur_eine_woche_herbstferien(
+        faq_entries,
+        vacation_colloquial,
+        assigns
+      )
+
+    preposition =
+      case assigns.location.name do
+        "Saarland" -> "im"
+        _ -> "in"
+      end
+
+    first_hit =
+      Enum.find(assigns.entries, fn entry -> entry.colloquial == vacation_colloquial end)
+
+    bonus_answer =
+      case Date.compare(first_hit.starts_on, assigns.requested_date) do
+        :gt ->
+          "Bis dahin sind es noch #{Date.diff(first_hit.starts_on, assigns.requested_date)} Tage."
+
+        _ ->
+          ""
+      end
+
+    faq_entries = [
+      %{
+        question:
+          "Wann beginnen die #{vacation_colloquial} #{preposition} #{assigns.location.name}?",
+        answer:
+          "Die #{vacation_colloquial} #{preposition} #{assigns.location.name} beginnen am #{Calendar.strftime(first_hit.starts_on, "%d.%m.%Y")} (ein #{FeriendatenWeb.LocationYearFaqComponents.wochentag(first_hit.starts_on)}). #{bonus_answer}"
+      }
+      | faq_entries
+    ]
+
+    faq_entries = [
+      %{
+        question:
+          "Wann sind #{assigns.year} #{vacation_colloquial} #{preposition} #{assigns.location.name}?",
+        answer:
+          "Die #{vacation_colloquial} #{preposition} #{assigns.location.name} beginnen am #{Calendar.strftime(first_hit.starts_on, "%d.%m.%Y")} (ein #{FeriendatenWeb.LocationYearFaqComponents.wochentag(first_hit.starts_on)}). #{bonus_answer}"
+      }
+      | faq_entries
+    ]
+
     faq_entries
   end
 end
