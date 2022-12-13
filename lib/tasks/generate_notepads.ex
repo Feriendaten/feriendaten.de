f74417fc2b357f0a7b1e202d5821560e2298c424
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2022-12-13 14:27:48 +0100

    Generate PDFs of notepads with vacation dates.

---
+++ lib/tasks/generate_notepads.ex
@@ -0,0 +1,191 @@
+defmodule Mix.Tasks.GenerateNotepads do
+  use Mix.Task
+  # import Ecto.Query
+
+  # alias Feriendaten.Repo
+
+  @requirements ["app.start"]
+
+  @shortdoc "Generate images of nodepads with vacation dates written on them."
+
+  @doc """
+  Generate images of nodepads with vacation dates written on them.
+
+  To generate the images, run:
+
+      mix generate_notepads
+  """
+
+  def run(_args) do
+    start_date = Date.utc_today()
+    end_date = Date.add(start_date, 14)
+    # end_date = Date.add(start_date, 365 * 2)
+
+    entries =
+      Feriendaten.Calendars.school_vacation_periods_for_germany(
+        start_date,
+        end_date
+      )
+
+    federal_states =
+      entries
+      |> Enum.map(fn l -> %{name: l.location_name, slug: l.location_slug} end)
+      |> Enum.uniq()
+      |> Enum.sort()
+
+    vacations =
+      entries
+      |> Enum.map(fn l -> l.colloquial end)
+      |> Enum.uniq()
+      |> Enum.sort()
+
+    dir = "/tmp/feriendaten.de-tmp"
+
+    case File.mkdir(dir <> "/Figures") do
+      :ok ->
+        write_all_files(dir, vacations, federal_states, start_date, end_date, entries)
+
+      {:error, :eexist} ->
+        write_all_files(dir, vacations, federal_states, start_date, end_date, entries)
+
+      _ ->
+        IO.puts("Could not create directory #{dir}")
+    end
+  end
+
+  def write_all_files(dir, vacations, federal_states, start_date, end_date, entries) do
+    write_notepadnotes_file(dir)
+    copy_notepad_background_image(dir)
+
+    for year <- start_date.year..end_date.year do
+      for federal_state <- federal_states do
+        for vacation <- vacations do
+          IO.puts("Generating image for #{vacation} #{federal_state.name} #{year}")
+
+          all_ferientermine =
+            Enum.filter(entries, fn x ->
+              x.location_name == federal_state.name && x.starts_on.year == year
+            end)
+            |> Feriendaten.Calendars.all_ferientermine_to_string()
+
+          write_latex_file(dir, vacation, federal_state.name, year, all_ferientermine)
+
+          file_name = Path.join([dir, "#{vacation}-#{federal_state.name}-#{year}.tex"])
+          _output = System.cmd("pdflatex", [file_name], cd: dir)
+        end
+      end
+    end
+
+    # compile_latex_file(dir, vacation, federal_state, year)
+    # convert_pdf_to_png(dir, vacation, federal_state, year)
+  end
+
+  def write_notepadnotes_file(dir) do
+    path = Path.join([dir, "notepadnotes.cls"])
+
+    {:ok, file} = :file.open(path, [:write])
+    :file.write(file, notepadnotes_content())
+  end
+
+  def write_latex_file(dir, vacation, federal_state, year, all_ferientermine) do
+    file_name = Path.join([dir, "#{vacation}-#{federal_state}-#{year}.tex"])
+
+    {:ok, file} = :file.open(file_name, [:write])
+    :file.write(file, tex_file_content(vacation, federal_state, year, all_ferientermine))
+  end
+
+  def tex_file_content(vacation, federal_state, year, all_ferientermine) do
+    ~S"""
+    % -- Preamble:
+    \documentclass{notepadnotes} % custom document class
+    \textsize{34}                % text size (notepad block)
+    \angle{3.55}                 % text rotation (angle in °)
+
+
+    % -- Begining of the document:
+    \begin{document}
+
+
+    \begin{notes}
+    headline\\
+
+    all_ferientermine
+    \end{notes}
+
+
+    \end{document}
+    """
+    |> String.replace("headline", "#{vacation}\\\\#{federal_state}\\\\#{year}")
+    |> String.replace("all_ferientermine", all_ferientermine)
+  end
+
+  def notepadnotes_content() do
+    ~S"""
+    % -- Class identification:
+    \NeedsTeXFormat{LaTeX2e}
+    \LoadClass[12pt]{article}
+
+    \ProvidesClass{notepadnotes}[2022/12/09 Notepad Notes: LaTeX custom class]
+
+    % -- Formatting:
+    \RequirePackage[paperheight=9.67in,paperwidth=14.5in,margin=1in]{geometry}
+    \RequirePackage{eso-pic,graphicx}
+    \RequirePackage[utf8]{inputenc}
+    \RequirePackage[T1]{fontenc}
+    \RequirePackage{aurical}
+    \RequirePackage[english,ngerman]{babel}
+    \RequirePackage{setspace}
+    \doublespacing
+    \setlength{\parindent}{0pt}
+    \setlength{\parskip}{6pt}
+    \RequirePackage{rotating}
+
+    % -- LaTeX macros:
+
+     % % Defining variables % %
+     % --- text size:
+     \def \textsize#1{\def\@textsize{#1}}
+     \def \@textsize {}
+
+      % --- text rotation (angle):
+     \def \angle#1{\def\@angle{#1}}
+     \def \@angle {}
+
+     % % environment to write on the notepad % %
+     \newenvironment{notes}
+     {
+       \vspace*{2.25cm} \hspace*{15cm}
+       \begin{turn}{\@angle}
+       \begin{minipage}[l]{9.75cm}
+         \fontfamily{augie}\selectfont
+     }
+     {
+           \bfseries
+       \end{minipage}
+       \end{turn}
+     }
+
+     % % To apply the figure back
+     \AtBeginDocument{
+       \AddToShipoutPictureBG*{\includegraphics[width=\paperwidth,height=\paperheight]{Figures/notepad-background.jpg}}
+
+       \fontsize{\@textsize}{30}\selectfont
+     }
+
+     % -- Alternative fonts:
+     % % augie
+         %\fontfamily{augie}\selectfont
+
+     % % pbsei
+         %\usepackage{pbsi}
+         %\bsifamily
+    """
+  end
+
+  def copy_notepad_background_image(dir) do
+    File.cp(
+      "#{Application.app_dir(:feriendaten)}/priv/latex/figures/notepad-background.jpg",
+      dir <> "/Figures/notepad-background.jpg"
+    )
+  end
+end
