12cfeadc0dce866c79a3c884f0dd9a80c7b69356
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2022-12-21 16:06:58 +0100

    Debugging

--- lib/feriendaten_web/controllers/error_html.ex
+++ lib/feriendaten_web/controllers/error_html.ex
@@ -8,7 +8,7 @@ defmodule FeriendatenWeb.ErrorHTML do
   #   * lib/feriendaten_web/controllers/error/404.html.heex
   #   * lib/feriendaten_web/controllers/error/500.html.heex
   #
-  # embed_templates "error/*"
+  embed_templates "error/*"
 
   # The default is to render a plain text page based on
   # the template name. For example, "404.html" becomes
